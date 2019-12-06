205177e4d6f343018e041d418658033d588f6b09
JEmbrace <1623143672@qq.com>
2019-12-06 10:21:32 +0800

    在App.vue组件中,使用element-ui提供的Table表格组件，在界面中添加并展示一个表格，同时将上传按钮添加到表格的最后一列

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,63 @@
 <template>
   <div id="app">
-    
+    <!-- element-ui Table表格组件 -->
+    <el-table
+        class="my-table"
+        :data="tableData"
+        stripe
+        style="width:725px;">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          width="180">
+        </el-table-column>
+        <!-- 添加一列附件管理（文件上传） -->
+        <el-table-column
+          prop="attach"
+          label="附件管理"
+          width="180">
+          <template>
+            <el-button size='small' type="primary" @click="dialogVisible = true">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路'
+      }]
+    }
+  }
 }
 </script>
 
