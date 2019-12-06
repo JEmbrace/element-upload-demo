24992ced9b15a75f9b05223f7942817356a10dd4
JEmbrace <1623143672@qq.com>
2019-12-06 10:27:47 +0800

    调整样式：给table添加一个边框并且将table居中显示

--- src/App.vue
+++ src/App.vue
@@ -66,8 +66,15 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 50px 30px;
+  text-align: center;
 }
 </style>
+<style scoped>
+#app .my-table{
+  display: inline-block;
+  border: 1px solid #EBEEF5;
+}
+</style>
+
